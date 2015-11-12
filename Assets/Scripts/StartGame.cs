506ade1ddaa6efe79ccf96e51f44238cb7ab5e19
Yadam17 <yyyyyyyyadam@gmail.com>
2015-11-12 13:55:58 -0600

    Working on loading the level selecter scene

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -3,13 +3,9 @@ using System.Collections;
 
 public class StartGame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void onClick()
+    {
+        
+        Application.LoadLevel("SelectLevelUI");
+    }
 }
