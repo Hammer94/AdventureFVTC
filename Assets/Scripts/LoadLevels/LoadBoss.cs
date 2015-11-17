b28071de19e76e319521e5144039b6905351f281
PaPeter <200020308@APPA105G02.fvtc.edu>
2015-11-17 17:23:47 -0600

    changes to lava texture to make it nicer

---
+++ Assets/Scripts/LoadLevels/LoadBoss.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadBoss : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            Application.LoadLevel("Boss Room");
+        }
+    }
+}
