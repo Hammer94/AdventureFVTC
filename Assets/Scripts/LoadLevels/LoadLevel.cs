0310254f0abce9309c2870a5a15396a3f5d0461a
Yadam17 <student@APPA105G15.fvtc.edu>
2015-11-17 13:33:57 -0600

    Loaded Levels
    Got the LevelSelect Scene to Load level1 and level2 scenes

---
+++ Assets/Scripts/LoadLevels/LoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            Application.LoadLevel("Level1");
+        }
+    }
+}
---
+++ Assets/Scripts/LoadLevels/LoadLevel2.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel2 : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            Application.LoadLevel("Level2");
+        }
+    }
+}
