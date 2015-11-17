b14956de30de638777072f43082406b603b8f072
PaPeter <200020308@APPA105G02.fvtc.edu>
2015-11-17 16:48:51 -0600

    added teleporter to lvl select

---
+++ Assets/Scripts/LoadLevels/LoadlvlSelect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadlvlSelect : MonoBehaviour 
+{
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            Application.LoadLevel("LevelSelect");
+        }
+    }
+}
