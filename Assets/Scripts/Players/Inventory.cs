97d47125d24d37e65a8086b55b5fd5bb2b1de240
Matt Dayton <dayton4626@fvtc.edu>
2015-11-19 13:55:39 -0600

    Inventory
    Inventory made

---
+++ Assets/Scripts/Players/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Inventory
+{
+
+    public static int Health = 6;
+    public static int Score = 0;
+    public static int Lvl1 = 0;
+    public static int Lvl2 = 0;
+    public static int OnEnter = 0;
+
+    public static void Reset()
+    {
+        Health = 6;
+        Score = 0;
+        
+    }
+
+    public static void setcurrentscore(int Score)
+    {
+        OnEnter = Score;
+    }
+}
