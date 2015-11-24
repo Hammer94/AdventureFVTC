55fb2c5ccb3d7ea02a2478a1e24de2753fd4e1f1
PaPeter <200020308@APPA105G09.fvtc.edu>
2015-11-24 17:44:46 -0600

    added Item
    messed with partical effect with item spinning have to turn off effect
    an hid item then add to invontory

---
+++ Assets/Scripts/Items/spin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spin : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () 
+    {
+        gameObject.transform.Rotate(0, 10, 0, Space.Self);
+	}
+}
