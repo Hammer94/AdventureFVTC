8bade8236bce9fb82f8f98bd14f92df7f7cfad5c
hearthfire <500133021@APPA105G17.fvtc.edu>
2015-12-08 14:13:47 -0600

    Working on water physics

---
+++ Assets/Scripts/Water Script/Swimming.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swimming : MonoBehaviour {
+
+    float WaterArea;
+    ParticleSystem Rabbit1;
+    bool ActiveUnderwater;
+    Color NormalColor;
+    Color UnderwaterColor;
+    
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        NormalColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+        UnderwaterColor = new Color(0.22f, 0.65f, 0.77f, 0.5f);
+        Rabbit1.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if ((transform.position.y < WaterArea) != ActiveUnderwater)
+        {
+            ActiveUnderwater = transform.position.y < WaterArea;
+            if (ActiveUnderwater) CheckUnderwater();
+            if (!ActiveUnderwater) CheckOnLand();
+        }
+
+        CheckOnLand();
+        CheckUnderwater();
+	}
+
+    void CheckOnLand()
+       
+        {
+            RenderSettings.fogColor = NormalColor;
+            RenderSettings.fogDensity = 0.002f;
+            Rabbit1.Stop();
+        }
+
+	   void CheckUnderwater()
+        {
+            RenderSettings.fogColor = UnderwaterColor;
+            RenderSettings.fogDensity = 0.03f;
+            Rabbit1.Play();
+        }
+}
