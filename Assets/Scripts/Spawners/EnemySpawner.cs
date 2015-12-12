0fd2eb05cbcef8111c4d3fd46f2d35aa2a580a71
Hammer94 <Quagmire20894@yahoo.com>
2015-12-12 05:31:53 -0600

    Updated enemyspawner

--- Assets/Scripts/Spawners/EnemySpawner.cs
+++ Assets/Scripts/Spawners/EnemySpawner.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 
 // @author  Ryan
-// @date    10 Dec 2015
+// @date    12 Dec 2015
 namespace AdventureFVTC {
     public class EnemySpawner:Spawner {
         [SerializeField] private bool spawnAtNight = false;
         [SerializeField] private bool onlySpawnAtNight = false;
         [SerializeField] private bool spawnOnlyOnce = false;     
-        public List<string> PatrolPoints = new List<string>();
-
+        [SerializeField] private List<string> PatrolPoints = new List<string>();
+      
         private EnemyController enemyController;
         private bool hasBeenSpawned = false;
         private bool readyToBeSpawned = false;
@@ -23,6 +23,11 @@ namespace AdventureFVTC {
             readyToBeSpawned = false;
         }
 
+        private void SetEnemyController()
+        {
+            
+        }
+
          public override void Reset() {
             reset = true;
             if (objectSpawned.gameObject != null) {
@@ -33,18 +38,6 @@ namespace AdventureFVTC {
         protected override void Start() {
             base.Start();
 
-            // Get this spawners enemyController.
-            enemyController = objectSpawned.GetComponent<EnemyController>();
-            // Set the patrol points.
-            foreach (string name in PatrolPoints)
-            {
-                GameObject point = GameObject.Find(name);
-                if (point != null)
-                {
-                    Transform t = point.GetComponent<Transform>();
-                    enemyController.Patrol.PatrolPoints.Add(t);
-                }
-            }
         }
 
         protected override void Respawn() {
@@ -84,7 +77,7 @@ namespace AdventureFVTC {
                     // If this spawner only spawns its enemy at night and it is night time.
                     if (onlySpawnAtNight && Services.Cycle.IsNight) 
                         Spawn();            
-                    else {
+                    else if (!onlySpawnAtNight) {
                         // If this spawner can spawn its enemy at night and at day, spawn whenever ready.
                         if (spawnAtNight) 
                             Spawn();                   
