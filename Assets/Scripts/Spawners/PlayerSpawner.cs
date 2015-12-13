d2bea1a15d6bd82bde882094e595141c62c002dd
Hammer94 <Quagmire20894@yahoo.com>
2015-12-12 22:07:06 -0600

    Fixed respawning camera transition

--- Assets/Scripts/Spawners/PlayerSpawner.cs
+++ Assets/Scripts/Spawners/PlayerSpawner.cs
@@ -4,9 +4,39 @@
 // @date    11 Dec 2015
 namespace AdventureFVTC {
     public class PlayerSpawner:Spawner {
+        [SerializeField] private GameObject deathMarker;
+
+        private GameObject markerClone;
         private bool viewingPanoramicView = true;
         private bool performingRespawn = true;
-        
+        private bool hasSetUp = false;
+        private bool destroyMarker = false;
+
+        private float destroyMarkerTime = 1.0f;
+        private float currentMakerTime = 0.0f;
+
+        private void DyingSetup() {
+            if (!hasSetUp) {
+                hasSetUp = true;             
+                markerClone = Object.Instantiate(deathMarker);
+                markerClone.name = "DeathMarker";
+                markerClone.transform.parent = Services.Run.Game.transform.Find("Players").transform;
+                markerClone.transform.position = Services.Run.Player.Camera.SubjectBehindDirection.transform.position;
+                Services.Run.Player.Camera.Subject = markerClone;
+                Services.Run.Player.Camera.SubjectBehindDirection = markerClone;
+            }
+        }
+
+        private void DestroyMarker() {
+            currentMakerTime += Time.deltaTime;
+            if (currentMakerTime >= destroyMarkerTime) {
+                destroyMarker = false;
+                hasSetUp = false;
+                currentMakerTime = 0.0f;              
+                Destroy(markerClone.gameObject);
+            }
+        }
+
         // This override ensures that the player spawner will never reset itself, as that functionality is already
         // present in the camera and PersistentPlayerStats.
         public override void Reset() {
@@ -19,11 +49,21 @@ namespace AdventureFVTC {
         }
 
         protected override void Update() {
-                if (Services.Run.Player.Character == null || Services.Run.Player.Character.Dead)
-                    Respawn();        
+            if (destroyMarker)
+                DestroyMarker();
+            
+            if (Services.Run.Player.Character == null || Services.Run.Player.Character.Dead) {
+                if (!viewingPanoramicView)
+                    DyingSetup();
+                Respawn();
+            }
+                            
         }
 
         protected override void Respawn() {
+            float distance;
+            float time;
+
             if (PersistentPlayerStats.LivesLeft > 0) {
                 if (reset) // If this is the first time this player has spawned this level attempt.           
                     reset = false; // Don't subtract any of their lives.             
@@ -32,12 +72,12 @@ namespace AdventureFVTC {
 
                 Services.Run.Player.Camera.IsSubjectChangeStillTransitioning = true;
                 
-                if (Services.Run.Player.Character != null) {
+                if (Services.Run.Player.Character != null) {                   
                     Services.Run.Player.Character.Dead = false;
                     Services.Run.Player.Character.MaxHealth = 3;
                     Services.Run.Player.Character.Health = Services.Run.Player.Character.MaxHealth;
                     SkinnedMeshRenderer rabbitRenderer = GameObject.FindGameObjectWithTag("RabbitRenderer").GetComponent<SkinnedMeshRenderer>();
-                    rabbitRenderer.material.color = Services.Run.Player.Character.InitialCharacterColor;
+                    rabbitRenderer.material.color = Services.Run.Player.Character.InitialCharacterColor;                
                 }
                 else {
                     Services.Run.Player.Character = Object.Instantiate(Services.Run.CharacterRoot);
@@ -51,26 +91,18 @@ namespace AdventureFVTC {
                     viewingPanoramicView = false;
                 else {
                     // Transition the subject using a time based on distance.
-                    float distance = Vector3.Distance(Services.Run.Player.Camera.transform.position, transform.position);
-                    float time = distance / 24.667f;
-                    if (time == 0)
-                        time = 0.1f;
+                    distance = Vector3.Distance(Services.Run.Player.Character.transform.position, Services.Run.Player.Camera.transform.position);
+                    time = distance / 24.667f;
+                    if (time < 4)
+                        time = 4f;
 
-                    Services.Run.Player.Camera.IsSubjectChangeStillTransitioning = false;
-                    GameObject PlayerSpawnerCameraPosition = GameObject.FindGameObjectWithTag("PlayerSpawnerCameraPosition").gameObject;
-                    Services.Camera.StartTransition(PlayerSpawnerCameraPosition.transform.position, time, true, "TransitionToPoint");
+                    Services.Camera.SetSubjectToPlayer(time);
+                    destroyMarker = true;                 
                 }                        
             }
             else if (PersistentPlayerStats.LivesLeft == 0) {
                 Services.Respawn.ResetAllSpawners(); // Reset all the spawners, removing their object from the gamespace and then putting them back in.
                 PersistentPlayerStats.GameOver(); // Reset this players stats and giving them back their lives, allowing them to respawn.
-
-                //// Transition the subject using a time based on distance.
-                //float distance = Vector3.Distance(Services.Run.Player.Camera.transform.position, transform.position);
-                //float time = distance / 24.667f;
-                //if (time == 0)
-                //    time = 0.1f;
-                //Services.Camera.SetSubjectToPlayer(time);
             }           
         }
     }
