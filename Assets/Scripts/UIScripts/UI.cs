6890d6e36b76520cef786189750df5aade74c8fd
Hammer94 <Quagmire20894@yahoo.com>
2015-12-14 04:05:31 -0600

    Updated Ui script and added a tracker for items collected
    Also added a present image that shows the player how many presents they
    have collected for the level they're on.

--- Assets/Scripts/UIScripts/UI.cs
+++ Assets/Scripts/UIScripts/UI.cs
@@ -10,11 +10,14 @@ namespace AdventureFVTC
     {
         GameObject lifeCounter;
         Text LifeCounter;
-        GameObject scoreCounter;
-        Text ScoreCounter;
+        //GameObject scoreCounter;
+        //Text ScoreCounter;
         GameObject filled1;
         GameObject filled2;
         GameObject filled3;
+        GameObject itemCounter;
+        Text ItemCounter;
+        GameObject presentsImage;
 
         // Use this for initialization
         void Start()
@@ -24,39 +27,62 @@ namespace AdventureFVTC
             filled3 = GameObject.FindGameObjectWithTag("filled3");
             lifeCounter = GameObject.FindGameObjectWithTag("Lives");
             LifeCounter = lifeCounter.GetComponent<Text>();
-            LifeCounter.text = "x" + PersistentPlayerStats.LivesLeft.ToString();
-            scoreCounter = GameObject.FindGameObjectWithTag("Score");
-            ScoreCounter = scoreCounter.GetComponent<Text>();
-            ScoreCounter.text = "Score: " + PersistentPlayerStats.GetScoreTotal.ToString();
-            //
-
+            LifeCounter.text = "x " + PersistentPlayerStats.LivesLeft.ToString();
+            itemCounter = GameObject.FindGameObjectWithTag("Items");
+            ItemCounter = itemCounter.GetComponent<Text>();
+            ItemCounter.text = "x " + PersistentPlayerStats.GetCurrentItems.ToString();
+            presentsImage = GameObject.FindGameObjectWithTag("Presents");
+            //scoreCounter = GameObject.FindGameObjectWithTag("Score");
+            //ScoreCounter = scoreCounter.GetComponent<Text>();
+            //ScoreCounter.text = "Score: " + PersistentPlayerStats.GetScoreTotal.ToString();
         }
 
         // Update is called once per frame
         void Update()
         {
-            LifeCounter.text = "x" + PersistentPlayerStats.LivesLeft.ToString();
+            LifeCounter.text = "x " + PersistentPlayerStats.LivesLeft.ToString();
 
-            if (Services.Run.Player.Character.Health == 3)
+            if (Application.loadedLevelName == "Level1") // We're currently on Level1.
+            {
+                if (PersistentPlayerStats.GetCurrentItems > PersistentPlayerStats.GetLevel1Total)
+                    ItemCounter.text = "x " + PersistentPlayerStats.GetCurrentItems.ToString() + "/" + PersistentPlayerStats.GetLevel1Goal.ToString();
+                else              
+                    ItemCounter.text = "x " + PersistentPlayerStats.GetLevel1Total.ToString() + "/" + PersistentPlayerStats.GetLevel1Goal.ToString();               
+            }
+            else if (Application.loadedLevelName == "Level2") // We're currently on Level1.
+            {
+                if (PersistentPlayerStats.GetCurrentItems > PersistentPlayerStats.GetLevel2Total)
+                    ItemCounter.text = "x " + PersistentPlayerStats.GetCurrentItems.ToString() + "/" + PersistentPlayerStats.GetLevel2Goal.ToString();
+                else
+                    ItemCounter.text = "x " + PersistentPlayerStats.GetLevel2Total.ToString() + "/" + PersistentPlayerStats.GetLevel2Goal.ToString();
+            }
+            else // Else we're on LeveSelect or the Boss Level
+            {
+                ItemCounter.text = ""; // Hide the text.
+                presentsImage.GetComponent<CanvasRenderer>().SetAlpha(0); // Hide the image.
+            }
+
+            
+            if (Services.Run.Player.Character != null && Services.Run.Player.Character.Health == 3)
             {
                 filled1.GetComponent<CanvasRenderer>().SetAlpha(1f);
                 filled2.GetComponent<CanvasRenderer>().SetAlpha(1f);
                 filled3.GetComponent<CanvasRenderer>().SetAlpha(1f);
 
             }
-            else if(Services.Run.Player.Character.Health == 2)
+            else if(Services.Run.Player.Character != null && Services.Run.Player.Character.Health == 2)
             {
                 filled1.GetComponent<CanvasRenderer>().SetAlpha(1f);
                 filled2.GetComponent<CanvasRenderer>().SetAlpha(1f);
                 filled3.GetComponent<CanvasRenderer>().SetAlpha(0f);
             }
-            else if (Services.Run.Player.Character.Health == 1)
+            else if (Services.Run.Player.Character != null && Services.Run.Player.Character.Health == 1)
             {
                 filled1.GetComponent<CanvasRenderer>().SetAlpha(1f);
                 filled2.GetComponent<CanvasRenderer>().SetAlpha(0f);
                 filled3.GetComponent<CanvasRenderer>().SetAlpha(0f);
             }
-            else if (Services.Run.Player.Character.Health == 0)
+            else if (Services.Run.Player.Character != null && Services.Run.Player.Character.Health == 0)
             {
                 filled1.GetComponent<CanvasRenderer>().SetAlpha(0f);
                 filled2.GetComponent<CanvasRenderer>().SetAlpha(0f);
